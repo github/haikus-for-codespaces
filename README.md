8243208e61deb26a0615cec6841c8250b0cd5e69
Allison Weins <allison.weins@microsoft.com>
2021-05-06 17:28:34 -0700

    Adding project files

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Haikus for Codespaces
+
+This is a quick node project for demoing Codespaces. It is based off of the [Azure node sample](https://github.com/Azure-Samples/nodejs-docs-hello-world). It's great!!!
---
+++ index.js
@@ -0,0 +1,14 @@
+let express = require('express');
+let app = express();
+let ejs = require('ejs');
+const haikus = require('./haikus.json');
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', {haikus: haikus});
+});
+
+app.listen(port);
